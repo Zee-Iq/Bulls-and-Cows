54aff0133cffb09ac84572b7b37b3b2e12bee9db
Zeeshan <zeeshan.iqbal@hotmail.de>
2021-11-22 22:07:19 +0100

    second commit of Bulls and Cows js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let secretNumbers = [];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // numbers to choose from
+let secretNumbers = []; // empty array --> random numbers will be pushed in here
 
+// Creating a for loop that generates and gives back a random 4 digit number. Numbers can be only be between 1 and 9
 for (let i = 0; i < 4; i++) {
-  let randomNumber = numbers.splice(Math.floor(Math.random() * 9), 1);
-  secretNumbers.push(randomNumber);
+  let randomNumber = numbers.splice(Math.floor(Math.random() * 9), 1); // using splice to randomize at what spot the random generated number will be printed to. 
+  secretNumbers.push(randomNumber); // pushing result into empty array already created in line 2 
 }
 console.log(secretNumbers);
 
@@ -16,14 +17,14 @@ body.append(showCounter);
 let input = document.createElement("input"); // the input has to be a four digit number
 input.type = "number";
 body.append(input);
-let button = document.createElement("button"); // creating an enter button
+let button = document.createElement("button"); // creating an enter button for the user
 button.textContent = "Enter";
 body.append(button);
 
 //event listener with a callback function to check wether it's bulls and/or cows
 
 let counter = 10; // giving player 10 tries
-button.addEventListener("click", function () {
+button.addEventListener("click", function () { // callback function here to run function after the user clicks the button or hits enter
   let bull = 0;
   let cow = 0;
   console.log(secretNumbers.join(""), input.value); //outputting a number instead of a string with .join("")
